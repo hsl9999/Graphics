68e274e94ac4999de6f7db49cb145a2f64d1f5a4
Alex Vázquez <alex.vazquez@unity3d.com>
2021-03-01 15:12:51 +0100

    Camera UI Drawer

---
+++ com.unity.render-pipelines.universal/Editor/Camera/UniversalRenderPipelineCameraUI.Drawer.cs
@@ -0,0 +1,9 @@
+namespace UnityEditor.Rendering.Universal
+{
+    //using CED = CoreEditorDrawer<UniversalRenderPipelineSerializedCamera>;
+
+    static partial class UniversalRenderPipelineCameraUI
+    {
+
+    }
+}
